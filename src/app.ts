3af3d53fe767a50b757f53c8001f5aaca3055d6f
claudiossilva93 <claudio.ssilva@outlook.com>
2023-10-02 20:56:12 -0300

    restaure request from typebot

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { sendWhatsAppMessage } from "./services/twilio";
+import { sendWhatsAppMessage, startFlow } from "./services/twilio";
 import dotenv from "dotenv";
 import { getOpenAICompletion } from "./services/openai";
 
@@ -23,6 +23,7 @@ app.post('/chat/send', async (req, res)=>{
 })
 
 app.post('/chat/receive', async (req, res)=>{
+    console.log(req);
     const twilioRequestBody  = req.body;
     const messageBody = twilioRequestBody.Body;
     const to = twilioRequestBody.From;
@@ -30,6 +31,17 @@ app.post('/chat/receive', async (req, res)=>{
         const completion = await getOpenAICompletion(messageBody);
         await sendWhatsAppMessage(to, completion)
         res.status(200).json({success: true, completion})
+    }catch(error){
+        res.status(500).json({success:false, error})
+    }
+})
+
+app.post('/flow/fluig', async (req, res)=>{
+    const twilioRequestBody  = req.body;
+    const to = twilioRequestBody.to;
+    try{
+        await startFlow(to, 'FW402b764c238776c5a34925c8f6ce4b61');
+        res.status(200).json({success: true})
     }catch(error){
         res.status(500).json({success:false, error})
     }
--- src/services/twilio.ts
+++ src/services/twilio.ts
@@ -9,7 +9,7 @@ const whatsAppPhoneNumber = process.env.WHATS_APP_PHONE_NUMBER;
 
 const client = twilio(accountSid, authToken);
 
-export const sendWhatsAppMessage = async (to: string, body: string): Promise<void> =>{
+const sendWhatsAppMessage = async (to: string, body: string): Promise<void> =>{
     try{
         await client.messages.create({
             to,
@@ -19,4 +19,24 @@ export const sendWhatsAppMessage = async (to: string, body: string): Promise<voi
     }catch(error){
         console.error(`Error send messaga to ${to}: ${error}`)
     }
+}
+
+const startFlow = async(to: string, flowId: string) : Promise<void>=> {
+    try{
+        const executionFlow = await client.studio.v2.flows(flowId)
+        .executions
+        .create({
+            to,
+            from: whatsAppPhoneNumber as string
+        });
+        console.log(executionFlow)
+    }catch(error){
+        console.error(`Error starting flow to ${to}: ${error}`)
+    }
+
+}
+
+export {
+    sendWhatsAppMessage,
+    startFlow
 }
